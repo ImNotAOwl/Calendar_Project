34c48f2a1f7afe2de5daadd03e99eb22530afac4
ImNotAOwl <siegel.matthieu@ymail.com>
2022-07-12 22:38:40 +0200

    add component select shift + insert into addEvents

--- src/components/AddEvent/AddEvent.js
+++ src/components/AddEvent/AddEvent.js
@@ -1,34 +1,50 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { gapi } from "gapi-script";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import ScrollSelect from "../ScrollSelect/ScrollSelect";
 
 const AddEvent = () => {
   const [value, onChange] = useState(new Date());
-  let [ rangeDate, setRangeDate ] = useState();
+  let [rangeDate, setRangeDate] = useState();
+  const shiftSelect = useRef();
 
   rangeDate = value;
-  console.log(rangeDate);
+  console.log(value.toLocaleString());
+  // console.log(shiftSelect.current.value);
 
   const addNewEvent = () => {
+    let summary, startDateTime, endDateTime;
+
+    switch (shiftSelect.current.value) {
+      case 1:
+        summary = "CHU Voiron Matin";
+        startDateTime = value.toLocaleString();
+        endDateTime = value.toLocaleString();
+        break;
+
+      default:
+        break;
+    }
+
     var newEvent = {
       summary: "Hello World",
       location: "",
       start: {
         dateTime: "2022-08-28T09:00:00-07:00",
-        timeZone: "America/Los_Angeles",
+        timeZone: "Europe/France",
       },
       end: {
         dateTime: "2022-08-28T17:00:00-07:00",
-        timeZone: "America/Los_Angeles",
+        timeZone: "Europe/France",
       },
       recurrence: [],
       attendees: [],
       reminders: {
         useDefault: false,
         overrides: [
-          { method: "email", minutes: 24 * 60 },
-          { method: "popup", minutes: 10 },
+          { method: "popup", minutes: 60 },
+          { method: "popup", minutes: 30 },
         ],
       },
     };
@@ -60,10 +76,12 @@ const AddEvent = () => {
 
   return (
     <div>
-      <Calendar
-        onChange={onChange}
-        value={value}
-        selectRange={true}
+      <Calendar onChange={onChange} value={value} />
+      <ScrollSelect
+        name="shift"
+        label="Selectionné un shift "
+        values={["SHift-Matin", "shift-soir"]}
+        ref={shiftSelect}
       />
       <button onClick={addNewEvent}>Envoyer</button>
     </div>
